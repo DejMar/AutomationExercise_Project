9679e1bb0ae0b06be610b9082f02c91ed70bd6be
DejMar <marjanovic.dejan@gmail.com>
2025-12-05 18:22:52 +0100

    update test steps

--- tests/E2E/homePage.spec.ts
+++ tests/E2E/homePage.spec.ts
@@ -22,41 +22,41 @@ test.describe('Home Page Tests', () => {
     });
 
     test('TC18 View Category Products', async ({ }) => {   
-        await homePage.selectCategory(Category.Kids.name, Category.Kids.subcategory.TopsAndShirts);
-        await homePage.verifyTitle(CategoryTitle.KidsTopsAndShirtsProducts);
-        await homePage.selectCategory(Category.Kids.name, Category.Kids.subcategory.Dress);
-        await homePage.verifyTitle(CategoryTitle.KidsDressProducts);
+        await testStep.log(homePage.selectCategory(Category.Kids.name, Category.Kids.subcategory.TopsAndShirts), 'Select Kids > Tops & Shirts');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.KidsTopsAndShirtsProducts), 'Verify Kids Tops & Shirts Products Title');
+        await testStep.log(homePage.selectCategory(Category.Kids.name, Category.Kids.subcategory.Dress), 'Select Kids > Dress');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.KidsDressProducts), 'Verify Kids Dress Products Title');
 
-        await homePage.selectCategory(Category.Men.name, Category.Men.subcategory.Tshirts);
-        await homePage.verifyTitle(CategoryTitle.MenTshirtsProducts);
-        await homePage.selectCategory(Category.Men.name, Category.Men.subcategory.Jeans);
-        await homePage.verifyTitle(CategoryTitle.MenJeansProducts);
+        await testStep.log(homePage.selectCategory(Category.Men.name, Category.Men.subcategory.Tshirts), 'Select Men > Tshirts');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.MenTshirtsProducts), 'Verify Men Tshirts Products Title');
+        await testStep.log(homePage.selectCategory(Category.Men.name, Category.Men.subcategory.Jeans), 'Select Men > Jeans');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.MenJeansProducts), 'Verify Men Jeans Products Title');
 
-        await homePage.selectCategory(Category.Women.name, Category.Women.subcategory.Tops);
-        await homePage.verifyTitle(CategoryTitle.WomenTopsProducts);
-        await homePage.selectCategory(Category.Women.name, Category.Women.subcategory.Dress);
-        await homePage.verifyTitle(CategoryTitle.WomenDressProducts);
-        await homePage.selectCategory(Category.Women.name, Category.Women.subcategory.Saree);
-        await homePage.verifyTitle(CategoryTitle.WomenSareeProducts);
+        await testStep.log(homePage.selectCategory(Category.Women.name, Category.Women.subcategory.Tops), 'Select Women > Tops');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.WomenTopsProducts), 'Verify Women Tops Products Title');
+        await testStep.log(homePage.selectCategory(Category.Women.name, Category.Women.subcategory.Dress), 'Select Women > Dress');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.WomenDressProducts), 'Verify Women Dress Products Title');
+        await testStep.log(homePage.selectCategory(Category.Women.name, Category.Women.subcategory.Saree), 'Select Women > Saree');
+        await testStep.log(homePage.verifyTitle(CategoryTitle.WomenSareeProducts), 'Verify Women Saree Products Title');
     });
 
     test('TC19 View Brand Products', async ({ }) => {
-        await homePage.clickBrand(Brand.Polo);
-        await homePage.verifyTitle(BrandTitle.Polo);
-        await homePage.clickBrand(Brand.Hm);
-        await homePage.verifyTitle(BrandTitle.Hm);
-        await homePage.clickBrand(Brand.Madame);
-        await homePage.verifyTitle(BrandTitle.Madame);
-        await homePage.clickBrand(Brand.MastAndHarbour);
-        await homePage.verifyTitle(BrandTitle.MastAndHarbour);
-        await homePage.clickBrand(Brand.Babyhug);
-        await homePage.verifyTitle(BrandTitle.Babyhug);
-        await homePage.clickBrand(Brand.AllenSollyJunior);
-        await homePage.verifyTitle(BrandTitle.AllenSollyJunior);
-        await homePage.clickBrand(Brand.KookieKids);
-        await homePage.verifyTitle(BrandTitle.KookieKids);
-        await homePage.clickBrand(Brand.Biba);
-        await homePage.verifyTitle(BrandTitle.Biba);
+        await testStep.log(homePage.clickBrand(Brand.Polo), 'Click Polo Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.Polo), 'Verify Polo Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.Hm), 'Click H&M Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.Hm), 'Verify H&M Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.Madame), 'Click Madame Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.Madame), 'Verify Madame Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.MastAndHarbour), 'Click Mast & Harbour Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.MastAndHarbour), 'Verify Mast & Harbour Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.Babyhug), 'Click Babyhug Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.Babyhug), 'Verify Babyhug Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.AllenSollyJunior), 'Click Allen Solly Junior Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.AllenSollyJunior), 'Verify Allen Solly Junior Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.KookieKids), 'Click Kookie Kids Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.KookieKids), 'Verify Kookie Kids Brand Title');
+        await testStep.log(homePage.clickBrand(Brand.Biba), 'Click Biba Brand');
+        await testStep.log(homePage.verifyTitle(BrandTitle.Biba), 'Verify Biba Brand Title');
     });
 
     test.skip('TC25 Verify Scroll Up using "Arrow" button and Scroll Down functionality', async ({ page }) => {
